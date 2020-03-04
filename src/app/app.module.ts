a42a3988a439b124d6ab9e5373198d89c482cfba
Benoit <benoit.henry.devweb@gmail.com>
2020-03-04 12:22:36 +0100

    ajout écran détail event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    EventListComponent,
+    EventDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: EventListComponent },
+      { path: 'detail', component: EventDetailComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
